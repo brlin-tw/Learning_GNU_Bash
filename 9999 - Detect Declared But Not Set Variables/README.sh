fcfd42cc029735bc9c53491fbfe745d43c024a94
林博仁 <Buo.Ren.Lin@gmail.com>
2017-02-12 03:11:54 +0800

    9999 - Detect Declared But Not Set Variables/README.sh - 簡化程式
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- 9999 - Detect Declared But Not Set Variables/README.sh
+++ 9999 - Detect Declared But Not Set Variables/README.sh
@@ -1,36 +1,6 @@
 #!/usr/bin/env bash
-######## Included files ########
+source "../0000 - Commons/Enable strict mode.source.bash"
 
-######## Included files ended ########
-
-######## File scope variable definitions ########
-# Defensive Bash Programming - not-overridable primitive definitions
-# http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
-readonly PROGRAM_FILENAME="$(basename "$0")"
-readonly PROGRAM_DIRECTORY="$(realpath --no-symlinks "$(dirname "$0")")"
-readonly PROGRAM_ARGUMENT_ORIGINAL_LIST="$@"
-readonly PROGRAM_ARGUMENT_ORIGINAL_NUMBER=$#
-
-## Unofficial Bash Script Mode
-## http://redsymbol.net/articles/unofficial-bash-strict-mode/
-# 將未定義的變數的參考視為錯誤
-set -u
-
-# Exit immediately if a pipeline, which may consist of a single simple command, a list, or a compound command returns a non-zero status.  The shell does not exit if the command that fails is part of the command list immediately following a `while' or `until' keyword, part of the test in an `if' statement, part of any command executed in a `&&' or `||' list except the command following the final `&&' or `||', any command in a pipeline but the last, or if the command's return status is being inverted with `!'.  If a compound command other than a subshell returns a non-zero status because a command failed while `-e' was being ignored, the shell does not exit.  A trap on `ERR', if set, is executed before the shell exits.
-#FIXME: ERR handler not working...don't know why
-exit_because_of_error() {
-	printf "Error: program interrupted because of error.\n" 1>&2
-	exit 1
-}
-trap exit_because_of_error ERR
-set -e
-
-# If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.
-set -o pipefail
-
-######## File scope variable definitions ended ########
-
-######## Program ########
 # Defensive Bash Programming - main function, program entry point
 # http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
 main() {
