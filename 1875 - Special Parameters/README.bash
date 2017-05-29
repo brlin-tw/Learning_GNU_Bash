9d34f656336adee91f34017d03e10ffd2da7d48a
林博仁 <Buo.Ren.Lin@gmail.com>
2017-05-29 19:40:30 +0800

    Add * Parameters
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ 1875 - Special Parameters/README.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+printf "Special Parameters\n"
+printf "=========================\n"
+printf "BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters\n"
+printf "\n"
+printf "\${0} = %s\n" "${0}"
+exit 0
