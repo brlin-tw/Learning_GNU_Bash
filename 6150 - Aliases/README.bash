e669857062a4081b4622a92f2dd5d540259d56ad
林博仁 <Buo.Ren.Lin@gmail.com>
2017-06-07 15:45:50 +0800

    Learn alias
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

---
+++ 6150 - Aliases/README.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Aliases
+# BASHDOC: Bash Features » Aliases
+
+## Aliases are not expanded in non-interactive mode by default, enable it
+shopt -s expand_aliases
+
+alias print_working_directory="pwd"
+print_working_directory
+
+printf "\n"
+
+a_very_long_name_function(){
+	echo "a_very_long_name_function called"
+}; alias long_name_function=a_very_long_name_function
+long_name_function
+
+exit 0
