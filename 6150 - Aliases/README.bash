3fef17b19748a266131488f12195c744052639c6
林博仁 <Buo.Ren.Lin@gmail.com>
2017-06-07 15:46:43 +0800

    Aliases: Add comment
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- 6150 - Aliases/README.bash
+++ 6150 - Aliases/README.bash
@@ -5,11 +5,13 @@
 ## Aliases are not expanded in non-interactive mode by default, enable it
 shopt -s expand_aliases
 
+## Alias of regular command
 alias print_working_directory="pwd"
 print_working_directory
 
 printf "\n"
 
+## Alias for function name
 a_very_long_name_function(){
 	echo "a_very_long_name_function called"
 }; alias long_name_function=a_very_long_name_function
