4af4199b5fb9c609b4e52a6dce37b179788cec1c
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-12 15:56:38 +0800

    6210 - Revisit Positional Parameters Traversal Using Arrays - Fix ShellCheck detected problems
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- 6210 - Revisit Positional Parameters Traversal Using Arrays/README.bash
+++ 6210 - Revisit Positional Parameters Traversal Using Arrays/README.bash
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-source "../9999 - Commons/Enable strict mode.source.bash"
+source '../9999 - Commons/Enable strict mode.source.bash'
 
 declare -ar commandline_argument_list_at=("${@}")
 declare -ir commandline_argument_quantity=${#}
 
 if [ "${commandline_argument_quantity}" -eq 0 ];then
-	printf "Script has no command-line argument.\n"
+	printf 'Script has no command-line argument.\n'
 else
 	declare -i counter
 	for ((counter = 0; counter < commandline_argument_quantity; ++counter)); do
-		printf "Command-line argument #%d: %s\n" "${counter}" "${commandline_argument_list_at[${counter}]}"
+		printf 'Command-line argument #%d: %s\n' "${counter}" "${commandline_argument_list_at[${counter}]}"
 	done
 fi
 
