95333e07cd9848a6aa536e4a1c2054605a556782
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-12 15:54:51 +0800

    1875 - Special Parameters: Fix SC detected problems
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- 1875 - Special Parameters/README.bash
+++ 1875 - Special Parameters/README.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-printf "Special Parameters\n"
-printf "=========================\n"
-printf "BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters\n"
-printf "\n"
-printf "\${0} = %s\n" "${0}"
+printf 'Special Parameters\n'
+printf '=========================\n'
+printf 'BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters\n'
+printf '\n'
+# ${0} is not an expansion
+# shellcheck disable=SC2016
+printf '${0} = %s\n' "${0}"
 exit 0
