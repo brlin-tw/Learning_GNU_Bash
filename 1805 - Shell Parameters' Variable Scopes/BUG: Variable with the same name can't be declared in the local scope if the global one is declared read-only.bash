a42f7a82f6cf9bafbe8dab33588376162ba34b85
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2020-11-15 19:31:07 +0800

    Shell Parameters' Variable Scopes: Add potential bug
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

---
+++ 1805 - Shell Parameters' Variable Scopes/BUG: Variable with the same name can't be declared in the local scope if the global one is declared read-only.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# BUG: Variable with the same name can't be declared in the local scope if the global one is declared read-only
+# https://lists.gnu.org/archive/html/bug-bash/2020-11/msg00065.html
+set \
+    -o errexit \
+    -o errtrace \
+    -o nounset
+
+declare -r variable_shadowed='(Value to be shadowed in the function)'
+
+check_values_of_variables_in_different_scope(){
+    local -r variable_shadowed="Another value"
+
+    printf 'In the %s context: \n' "${FUNCNAME[0]}"
+    printf 'variable_shadowed = %s\n' "${variable_shadowed}"
+}
+check_values_of_variables_in_different_scope
